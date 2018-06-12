cd19b11677a7d185f08cab00535a50aac4806c66
Praise <pnanvuma@gmail.com>
2018-06-12 19:43:04 +0300

    stup cookies

--- js/3dsc.js
+++ js/3dsc.js
@@ -1,22 +1,83 @@
-var getDates = function(target){
-    // sets the date of clicked/selected button
-    minDate = null;
-    maxDate = null
-    if(target == "tDate"){
-        minDate = maxDate = Date.today();
-        target =  minDate.toString('dd.MM.yyyy');
-    }else if (target == "yDate"){
-        minDate = maxDate = Date.parse("yesterday");
-        target =  minDate.toString('dd.MM.yyyy'); 
-    }else if(target == "week"){
+var showPickerFields = function(){
+    //pick variables from cookie
+    // 1-Setup Picker Fields, 2-Hide Picker Display, 3-Show Picker Fields
+    var rDates = Cookies.getJSON("rDates");
+    console.log(rDates);
+    var fromDate = rDates.fDate;
+    var toDate = rDates.tDate;
+    //---Time Picker JS-----
+    $("#fromDate").val(fromDate);
+    $("#toDate").val(toDate);
+    console.log(fromDate);
+    $("#fromDate").datepicker({'dateFormat': "dd.mm.yy"});
+    // $("#fromDate").datepicker({
+    //     'defaultDate': Date.parse(fromDate),
+    //     'setDate': Date.parse(fromDate)
+    // });
+    $("#toDate").datepicker({'dateFormat': "dd.mm.yy"});
+    // $("#toDate").datepicker({
+    //     'defaultDate': '04.06.2018',
+    //     'setDate': toDate
+    // });
+   
+    $(".picker-display").hide();
+    $(".picker-fields").show();
+}
 
-    }else if (target == "month"){
-
-    }else if (target == "custom"){
-
+var showPickerDisplays = function(period, sDate){
+    //pick variables from cookie
+    // Setup Picker Display, 2-Hide Picker Fields, 3-Show Picker Display
+    console.log(Cookies.getJSON("rDates"));
+    if(period == "wDate" || period == "mDate"){
+        $("#selDate").addClass("form-control-sel");
+    }else{
+        $("#selDate").removeClass("form-control-sel");
     }
-    $("#selDate").val(target);
-    // alert("target is " + target); 
+    $("#selDate").val(sDate);
+    $(".picker-fields").hide();
+    $(".picker-display").show();
+}
+
+var setCookies =  function(k,v){
+    ck = Cookies.get(k);
+    if(ck != null){
+        Cookies.remove(k)
+    }
+    Cookies.set(k,v);
+}
+
+var getDates = function(target){
+    // sets the date &   type of clicked/selected button in a coookie
+    setCookies("period", target);
+    if(target == "tDate"){
+        minDate = maxDate = Date.today().toString('dd.MM.yyyy');
+        setCookies("rDates", {fDate: minDate, tDate: maxDate});
+        setCookies("selDates", minDate);
+        showPickerDisplays(target,minDate);
+    }else if (target == "yDate"){
+        minDate = maxDate = Date.parse("yesterday").toString('dd.MM.yyyy');
+        setCookies("rDates", {fDate: minDate, tDate: maxDate});
+        setCookies("selDates", minDate);
+        showPickerDisplays(target,minDate); 
+    }else if(target == "wDate"){
+        minDate = Date.today().last().week().toString('dd.MM.yyyy');
+        maxDate = Date.today().toString('dd.MM.yyyy');
+        sDate = minDate + "-" + maxDate;
+        setCookies("rDates", {fDate: minDate, tDate: maxDate});
+        setCookies("selDates", sDate);
+        showPickerDisplays(target,sDate); 
+    }else if (target == "mDate"){
+        minDate = new Date().moveToFirstDayOfMonth().toString('dd.MM.yyyy');
+        maxDate = new Date().moveToLastDayOfMonth().toString('dd.MM.yyyy');
+        sDate = minDate + "-" + maxDate;
+        setCookies("rDates", {fDate: minDate, tDate: maxDate});
+        setCookies("selDates", sDate);
+        showPickerDisplays(target, sDate); 
+    }else if (target == "cDate"){
+        showPickerFields();
+        Cookies.remove("selDates");
+        // Cookies.remove("rDates");
+    }
 };
 
 var getNextDate =  function(target){
@@ -32,6 +93,7 @@ var getPreviousDate =  function(target){
 
 var loadReportTemplate = function(rTemp){
     console.log(rTemp);
+    //--first load template with a progress bar then update it with info fetched if any
 };
 
 $(document).ready( function () {
@@ -42,10 +104,8 @@ $(document).ready( function () {
         ]
     } );
 
-
-    //---Time Picker JS-----
-    $("#fromDate").datepicker();
-    $("#toDate").datepicker();
+    var aTarget = $(".active").children("input").prop("id");
+    getDates(aTarget);
 
     //--Get Dates/ranges of clicked period buttons and display them accordingly
     $("#dTargets label").on('click',function(){
